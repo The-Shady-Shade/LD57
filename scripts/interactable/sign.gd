6bf847c61d6559beef3559f841237f244c70991d
ShadyShade <theshadyshade@mail.ru>
2025-04-09 21:55:19 +0300

    Setup GitHub Pages

--- scripts/interactable/sign.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@export var text: Array[String] = []
-var finished: bool = false
-
-func _on_interaction_area_body_entered(body: Node2D) -> void:
-	if body == global.player && !finished:
-		dialog_manager.start_dialog(text, global_position)
-		finished = true
--- scripts/singletones/global.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-var player: CharacterBody2D
-var computers_fixed: int = 0
-var destroy: bool = false
